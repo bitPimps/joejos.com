f080562eb1a2f8d6d710e609a7910917acda4d66
bitPimps <bitpimps@gmail.com>
2019-05-24 16:01:22 -0500

    Favicon and css updates
    Adding favicon and psd logo

---
+++ wwwRoot/inc/footer.php
@@ -0,0 +1,49 @@
+<footer class="footer">
+			<section class="light">
+				<div class="container py-5">
+					<div class="row">
+						<div class="col-md-4">
+							<h2>About Us</h2>
+							<p>In pharetra, arcu eu tincidunt laoreet, leo risus tempus nisl, id pretium erat dolor sed nunc.</p>
+						</div>
+						<div class="col-md-4">
+							<h2>Our Features</h2>
+							<p>In pharetra, arcu eu tincidunt laoreet, leo risus tempus nisl, id pretium erat dolor sed nunc.</p>
+						</div>
+						<div class="col-md-4">
+							<h2>Enroll Now</h2>
+							<p>In pharetra, arcu eu tincidunt laoreet, leo risus tempus nisl, id pretium erat dolor sed nunc.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="dark">
+				<div class="container">
+					<div class="row pt-3">
+						<div class="col-md-6 contact-info">
+							<p><small>Joe-Jo's Fish &amp; Chicken | <a href="tel:+13143008481">314.300.8481</a> | <a href="mailto:emailme@gmail.com">emailme@gmail.com</a></small></p>
+						</div>
+						<div class="col-md-6 copyright">
+							<p><small>Copyright &copy; <?php echo date("Y") ?> Joe-Jo's Fish &amp; Chicken - All Rights Reserved.</small></p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</footer>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+		<script>
+			WebFontConfig = {
+				google: {
+					families: ['Muli','Nobile|','Open+Sans+Condensed']
+				}
+			};
+			(function(d) {
+				var wf = d.createElement('script'), s = d.scripts[0];
+				wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+				wf.async = true;
+				s.parentNode.insertBefore(wf, s);
+			})(document);
+		</script>
