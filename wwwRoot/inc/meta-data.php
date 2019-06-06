6b35039534f23b191d49af0ac977155c6eb1cd31
bitPimps <bitpimps@gmail.com>
2019-06-06 15:49:37 -0500

    Cover page
    Cover page to show menu asap

--- wwwRoot/inc/meta-data.php
+++ wwwRoot/inc/meta-data.php
@@ -14,4 +14,5 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli|Nobile|Open+Sans+Condensed:300&display=swap">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-		<link rel="stylesheet" href="/caua/css/main.css?<?php echo(rand(10,10000)); ?>" />
+		<link rel="stylesheet" href="/joejos/css/main.css" />
+		<link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/cover/">
---
+++ wwwRoot2/index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Joe-Jo's Fish &amp; Chicken</title>
+		<meta name="description" content="">
+		<?php include_once("inc/meta-data.php"); ?>
+	</head>
+
+	<body>
+		<?php include_once("inc/nav-main.php"); DrawNavMain("Home"); ?>
+		<header class="hero">
+			<div class="container">
+				<h1>Joe-Jo's</h1>
+				<div class="sub-heading">
+					<p>Fish &amp; Chicken</p>
+				</div>
+			</div>
+		</header>
+		<main role="main">
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>Our Philosophy</h1>
+						<p class="lead">At Children Are Unique Academy, we believe in creating positive developmental interactions between children and their surroundings.</p>
+						<p><strong>The early years of childhood are the most influential in a child’s life.</strong><br>
+							We believe that young children learn most effectively when they have opportunities to explore their surroundings and experience a variety of play. We offer a nurturing, dynamic and diverse environment that encourages a love of learning and connection with others.</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid" src="img/pages/philosophy.jpg" alt="Our Philosophy">
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-4">
+						<img class="img-fluid" src="img/pages/mission.jpg" alt="Our Mission">
+					</div>
+					<div class="col-md-8">
+						<h1>Our Mission</h1>
+						<p class="lead">Children Are Unique Academy gently encourages children to discover their individual strengths and unique talents.</p>
+						<p><strong>Each child possesses a marvelous capacity to learn.</strong><br>
+							Not only to we provide quality care, but we also go the extra mile to help every individual child reach his or her full potential. Our teachers and caregivers lead by example, demonstrating that learning is best accomplished in spaces that are emotionally secure, physically safe, and all-around fun!</p>
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>Our Vision</h1>
+						<p class="lead">Children Are Unique Academy strives to be a community where caregivers and families work together to nurture happy, healthy, and bright children.</p>
+						<p><strong>We build trust with the families we support.</strong><br>
+							We aim to create an environment where families feel confident about their child’s learning and development. By working in partnership with one another, we are able to give all children a sense of dignity, respect, and most of all, community.</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid" src="img/pages/vision.jpg" alt="Our Vision">
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-3 text-center">
+						<span class="pb-5 fa-stack fa-5x">
+							<i class="fa fa-circle fa-stack-2x red"></i>
+							<i class="fa fa-university fa-stack-1x white"></i>
+						</span>
+						<h1 class="red">Qualified Teachers</h1>
+						<p>Our teachers are compassionate, trained caregivers.</p>
+					</div>
+					<div class="col-md-3 text-center">
+						<span class="pb-5 fa-stack fa-5x">
+							<i class="fa fa-circle fa-stack-2x yellow"></i>
+							<i class="fa fa-apple fa-stack-1x white"></i>
+						</span>
+						<h1 class="yellow">Nutritious Meals</h1>
+						<p>We offer breakfast, lunch, and snacks throughout the day.</p>
+					</div>
+					<div class="col-md-3 text-center">
+						<span class="pb-5 fa-stack fa-5x">
+							<i class="fa fa-circle fa-stack-2x green"></i>
+							<i class="fa fa-child fa-stack-1x white"></i>
+						</span>
+						<h1 class="green">Flexible Scheduling</h1>
+						<p>We operate 6 AM-9 PM Sunday-Saturday, offering morning, daytime and evening care.</p>
+					</div>
+					<div class="col-md-3 text-center">
+						<span class="pb-5 fa-stack fa-5x">
+							<i class="fa fa-circle fa-stack-2x blue"></i>
+							<i class="fa fa-smile-o fa-stack-1x white"></i>
+						</span>
+						<h1 class="blue">Facility Features</h1>
+						<p>We have two buildings and an outdoor play area for children ages 6 weeks to 12 years old.</p>
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-6">
+						<img class="img-fluid mx-auto d-block" src="img/pages/program.jpg" alt="Program">
+						<h1 class="my-5 text-center">Program</h1>
+						<p>Open Sunday-Saturday from 6 AM – 9 PM, we offer all-day care for children who are 6 weeks old to preschool-aged. We have one building for infants and toddlers, and a larger building for preschoolers. We also offer before and after-school care for elementary and middle school children (ages 6-12) in our larger building. We consult with families to create a program that makes sense for them and their busy schedules.</p>
+					</div>
+					<div class="col-md-6">
+						<img class="img-fluid mx-auto d-block" src="img/pages/history.jpg" alt="A Brief History">
+						<h1 class="my-5 text-center">A Brief History</h1>
+						<p>Children Are Unique Academy was founded in 2017 by Annette Smoote and Sherri Cole, who believe that all children are unique no matter their race, ethnicity or gender. Both women had over 24 years of experience working with children, and so they used their combined skills to form and operate their own childcare center. Annette and Sherri wanted to create a place where parents knew their children would be safe and well taken care of. Today, Annette and Sherri are fully committed to running a top-quality daycare center and go above and beyond to make sure everything runs smoothly.</p>
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-12">
+						<img class="mx-auto d-block w-50" src="/caua/img/logos/children-are-unique-academy.jpg" alt="Children Are Unique Academy">
+					</div>
+				</div>
+			</section>
+		</main>
+		<?php include_once("inc/footer.php"); ?>
+	</body>
+</html>
